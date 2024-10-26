0456c9fd2720683317f5895e89b315cd50cc5a91
SamudraUduwaka <134803940+SamudraUduwaka@users.noreply.github.com>
2024-10-26 12:54:05 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grade_mcq",
+    version="1.0",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "grade_mcq=grade_mcq:main",
+        ],
+    },
+    install_requires=[
+        "opencv-python",
+        "numpy",
+        "matplotlib",
+        "pandas"
+    ],
+)
